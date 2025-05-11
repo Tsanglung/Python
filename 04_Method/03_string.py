3cecc293ce2dc95ba8cd147b13e6c95dead6a261
Tsanglung <ab1726531@gmail.com>
2025-05-11 11:35:40 +0800

    字串操作轉換

---
+++ 04_Method/03_string.py
@@ -0,0 +1,10 @@
+a = str(123)  # int to string
+b = chr(123)  # unicode 字元
+c = ord(b)  # unicode 轉回 對應編碼數
+print(a, b, c)
+a = ascii("早安")  # 轉 ascii 碼
+print(a)
+a = bin(123)  # 2 進位 字串
+b = oct(123)  # 8進位
+c = hex(123)  # 16進位
+print(a, b, c)
